937d78b029e528d8fad8f341f50ffdfe87b248ef
Dimk <shamparov@gmail.com>
2019-07-08 10:32:55 +0300

    javascript-1: multi parentheses arrow sum2()

--- javascript/1.js
+++ javascript/1.js
@@ -1,11 +1,34 @@
+function sum2(a) {
+    return (b) => {
+        if (typeof b !== 'undefined') {
+            a = a + b;
+            return sum2(a);
+        }
+        return a;
+    }
+}
+
+// Some tests
+console.log(sum2(1)());
+console.log(sum2(1)(2)());
+console.log(sum2(1)(2)(4)());
+console.log(sum2(1)(2)(4));
+console.log(sum2(0)(-1.1)());
+console.log(sum2(0)('1')());
+console.log(sum2(null)('tests')(1)());
+console.log(sum2(null)(true)());
+console.log(sum2(true)(true)());
+console.log(sum2(true)(1)());
+
+//--
+
 function sum(a) {
     return function innerSum(b) {
         if (typeof b !== 'undefined') {
             a = a + b;
             return innerSum;
-        } else {
-            return a;
         }
+        return a;
     }
 }
 
